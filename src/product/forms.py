2dafe133d7a81cbfa82cb0be707c1b5a13a7ae01
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2022-08-01 03:09:31 +0600

    complete all requirement

--- src/product/forms.py
+++ src/product/forms.py
@@ -1,14 +1,26 @@
 from django.forms import forms, ModelForm, CharField, TextInput, Textarea, BooleanField, CheckboxInput
 
-from product.models import Variant
+from product.models import *
 
 
 class VariantForm(ModelForm):
     class Meta:
         model = Variant
         fields = '__all__'
-        widgets = {
-            'title': TextInput(attrs={'class': 'form-control'}),
-            'description': Textarea(attrs={'class': 'form-control'}),
-            'active': CheckboxInput(attrs={'class': 'form-check-input', 'id': 'active'})
-        }
+      
+        
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+      
+class ProductVariantForm(ModelForm):
+    class Meta:
+        model = ProductVariant
+        fields = '__all__'
+        
+class ProductVariantPriceForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+         
